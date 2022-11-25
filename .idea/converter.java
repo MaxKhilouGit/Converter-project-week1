cafc2dca2602ea4ed396c39c7c201214c409aba7
Andrei Ciuperca <andrei1.ciuperca.gmail.com>
2022-11-25 13:34:38 -0600

    created 1

---
+++ .idea/converter.java
@@ -0,0 +1,6 @@
+public static double convertGramsToOunces(double grams) {return (double) (grams*0.035274)}
+
+public static double convertGramsToKilos(double grams) {return (double) (grams*1000)}
+
+public static double convertGramsToPounds(double grams) {return (double) (grams*0.00220462)}
+        }
