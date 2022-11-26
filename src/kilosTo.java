0a7cb5160ffc86ece119b7e4efd70dc750e6df7a
Josh <joshuasevendavid@gmail.com>
2022-11-26 08:36:33 -0500

    kilosTo class optimized

--- src/kilosTo.java
+++ src/kilosTo.java
@@ -4,20 +4,19 @@ class kilosTo extends convert{
     kilosTo(Scanner sc){
         System.out.println("Enter measurement to convert it into: 1. pounds; 2. grams; 3. ounces");
         uOption = sc.nextInt();
+        System.out.println("Enter amount of kilograms:");
+        kilograms = sc.nextDouble();
         //user options direct to appropriate methods
         switch(uOption){
-            case 1: kilosToPounds(sc);break;
-            case 2: kilosToGrams(sc);break;
-            case 3: kilosToOunces(sc);break;
+            case 1: kilosToPounds(kilograms);break;
+            case 2: kilosToGrams(kilograms);break;
+            case 3: kilosToOunces(kilograms);break;
         }
     }
     //KiloGrams TO conversion and return methods======================
     // to pounds
-    public String kilosToPounds(Scanner sc){
-        System.out.println("Enter amount of kilograms:");
-        kilograms = sc.nextDouble();
+    public String kilosToPounds(double kilograms){
         pounds = kilograms * 2.20462;
-
         if(grams == 1) {
             result = kilograms + kiloStr + equals + pounds + poundsStr + ".";
         }else{
@@ -26,11 +25,8 @@ class kilosTo extends convert{
         return result;
     }
     //============to Grams
-    public String kilosToGrams(Scanner sc){
-        System.out.println("Enter amount of kilograms:");
-        kilograms = sc.nextDouble();
+    public String kilosToGrams(double kilograms){
         grams = kilograms * 1000;
-
         if(pounds == 1) {
             result = kilograms + kiloStr + equals + grams + gramsStr + ".";
         }else{
@@ -39,11 +35,8 @@ class kilosTo extends convert{
         return result;
     }
     // to Ounces=======================
-    public String kilosToOunces(Scanner sc){
-        System.out.println("Enter amount of kilograms:");
-        kilograms = sc.nextDouble();
+    public String kilosToOunces(double kilograms){
         ounces = kilograms * 35.27;
-
         if(pounds == 1) {
             result = kilograms + kiloStr + equals + ounces + ouncesStr + ".";
         }else{
