c3617e23c714ecf6ed3ae12d110b2b6f1b054b3d
Josh <joshuasevendavid@gmail.com>
2022-11-26 08:38:24 -0500

    poundsTo class optimized

--- src/poundsTo.java
+++ src/poundsTo.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 class poundsTo extends convert{
+    //================================
     poundsTo(Scanner sc){
         System.out.println("Enter measurement to convert it into: 1. ounces; 2. kilograms; 3. grams");
         uOption = sc.nextInt();
+        System.out.println("Enter amount of pounds:");
+        pounds = sc.nextDouble();
         //
         switch(uOption){
-            case 1: poundsToOz(sc);break;
-            case 2: poundsToKG(sc);break;
-            case 3: poundsToGr(sc);break;
+            case 1: poundsToOz(pounds);break;
+            case 2: poundsToKG(pounds);break;
+            case 3: poundsToGr(pounds);break;
         }
     }
     //POUNDS TO conversion and return methods======================
     // to Ozs
-    public String poundsToOz(Scanner sc){
-        System.out.println("Enter amount of pounds:");
-        pounds = sc.nextDouble();
+    public String poundsToOz(double pounds){
         ounces = pounds * 16.0;
         if(pounds == 1) {
             result = pounds + poundStr + equals + ounces + ouncesStr + ".";
@@ -24,11 +25,8 @@ class poundsTo extends convert{
         return result;
     }
     // to KGs========================
-    public String poundsToKG(Scanner sc){
-        System.out.println("Enter amount of pounds:");
-        pounds = sc.nextDouble();
+    public String poundsToKG(double pounds){
         kilograms = pounds * 0.453592;
-
         if(pounds == 1) {
             result = pounds + poundStr + equals + kilograms + kilosStr + ".";
         }else{
@@ -37,11 +35,8 @@ class poundsTo extends convert{
         return result;
     }
     // to Grams=======================
-    public String poundsToGr(Scanner sc){
-        System.out.println("Enter amount of pounds:");
-        pounds = sc.nextDouble();
+    public String poundsToGr(double pounds){
         grams = pounds * 453.592;
-
         if(pounds == 1) {
             result = pounds + poundStr + equals + grams + gramsStr + ".";
         }else{
