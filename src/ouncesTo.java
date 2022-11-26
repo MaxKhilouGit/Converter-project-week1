058738837246fdff596a45e2fce1943946d3e93c
Josh <joshuasevendavid@gmail.com>
2022-11-26 08:37:28 -0500

    ouncesTo class optimized

--- src/ouncesTo.java
+++ src/ouncesTo.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 class ouncesTo extends convert {
+    //================================
     ouncesTo(Scanner sc){
         System.out.println("Enter measurement to convert it into: 1. pounds; 2. kilograms; 3. grams");
         uOption = sc.nextInt();
+        System.out.println("Enter amount of ounces:");
+        ounces = sc.nextDouble();
         //
         switch(uOption){
-            case 1: ouncesToLB(sc);break;
-            case 2: ouncesToKg(sc);break;
-            case 3: ouncesToGr(sc);break;
+            case 1: ouncesToLB(ounces);break;
+            case 2: ouncesToKg(ounces);break;
+            case 3: ouncesToGr(ounces);break;
         }
     }
     //OUNCES TO conversion and return methods======================
     // to LBs
-    public String ouncesToLB(Scanner sc){
-        System.out.println("Enter amount of ounces:");
-        ounces = sc.nextDouble();
+    public String ouncesToLB(double ounces){
         pounds = ounces * 0.0625;
         if(pounds == 1) {
             result = ounces + ounceStr + equals + pounds + poundsStr + ".";
@@ -25,11 +26,8 @@ class ouncesTo extends convert {
         return result;
     }
     // to KGs========================
-    public String ouncesToKg(Scanner sc){
-        System.out.println("Enter amount of ounces:");
-        ounces = sc.nextDouble();
+    public String ouncesToKg(double ounces){
         kilograms = ounces * 0.0283495;
-
         if(pounds == 1) {
             result = ounces + ounceStr + equals + kilograms + kilosStr + ".";
         }else{
@@ -38,11 +36,8 @@ class ouncesTo extends convert {
         return result;
     }
     // to Grams=======================
-    public String ouncesToGr(Scanner sc){
-        System.out.println("Enter amount of ounces:");
-        ounces = sc.nextDouble();
+    public String ouncesToGr(double ounces){
         grams = ounces * 28.34952;
-
         if(pounds == 1) {
             result = ounces + ounceStr + equals + grams + gramsStr + ".";
         }else{
